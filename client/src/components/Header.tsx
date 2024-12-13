5693a17194bfde10bda02af4a0a47ebc72bd1089
Xavier Figueroa <xfigueroa28@gmail.com>
2024-12-12 21:59:51 -0500

    Create new button in header to signup and update the login/signup modal code

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,10 +2,13 @@ import { useState } from 'react';
 import TTNGLOGO from '../assets/images/TTNG-LOGO.png';
 import { Link } from 'react-router-dom';
 import { LoginModal } from './LoginModal';
+import { CreateProfileModal } from './CreateProfileModal';
+
 
 
 export const Header: React.FC = () => {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
 
   const openLoginModal = () => {
     setIsLoginModalOpen(true);
@@ -15,11 +18,24 @@ export const Header: React.FC = () => {
     setIsLoginModalOpen(false);
   };
 
+  const openSignupModal = () => {
+    setIsSignupModalOpen(true);
+  };
+
+  const closeSignupModal = () => {
+    setIsSignupModalOpen(false);
+  };
+
+
     return (
         <div className='header'>
           <div className='top-header'> 
           <a href="/"><img className='logo' src={TTNGLOGO}></img></a>
-          <button className='login-btn' onClick={openLoginModal}>Login</button>
+          <div className='header-btn'>
+            <button className='login-btn' onClick={openLoginModal}>Login</button>
+            <button className='signup-btn' onClick={openSignupModal}>Sign Up</button>
+          </div>
+
           </div> 
           <nav className='nav-bar'>
              <Link to="/">Home</Link>
@@ -28,6 +44,7 @@ export const Header: React.FC = () => {
              <Link to="/meals">Meals</Link>
           </nav>
           {isLoginModalOpen && <LoginModal onClose={closeLoginModal} />}
+          {isSignupModalOpen && <CreateProfileModal onClose={closeSignupModal} />}
 
         </div>
     );
--- client/src/components/LoginModal.tsx
+++ client/src/components/LoginModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './LoginModal.module.css';
 
 interface LoginModalProps {
     onClose: () => void;
@@ -6,46 +7,25 @@ interface LoginModalProps {
 
 export const LoginModal: React.FC<LoginModalProps> = ({onClose}) => {
     return (
-        <div className='login-modal'>
-            <div className='login-modal-content'>
-                <button className="close-btn" onClick={onClose} ></button>
+        <div className={styles['login-modal']}>
+            <div className={styles['login-modal-content']}>
+                
                 <form action="submit" method="POST">
-                    <label htmlFor="name">Full Name:</label>
-                    <input type="text" name="name" id="name" placeholder="Enter your fullname here" required/>
-
+                    
                     <label htmlFor="email">Email Address:</label>
                     <input type="email" name="email" id="email" placeholder="Enter your email here" required/>
 
                     <label htmlFor="password">Password:</label>
                     <input type="password" name="password" id="password" placeholder="Enter your password here" required/>
 
-                    <label htmlFor="age">Age:</label>
-                    <input type="number" name="age" id="age" placeholder="Enter your age here" min={1} max={120} required/>
-
-                    <label htmlFor="gender">Gender:</label>
-                    <select id="gender" name="gender" required>
-                        <option value="">Select</option>
-                        <option value="male">Male</option>
-                        <option value="female">Famale</option>
-                        <option value="other">Other</option>
-                        <option value="prefer_not_to_say">Prefer not to say</option>                        
-                    </select>
-
-                    <label htmlFor="fitness-goals">Fitness Goals</label>
-                    <textarea name="fitness-goals" id="fitness-goals" placeholder="Describe your fitness goals (e.g., lose weight, gain muscle, improve endurance)" rows={4} required></textarea>
-                    
-                    <label htmlFor="exercise-preferences">Exercise Preferences:</label>
-                    <select name="exercise-preferences" id="exercice-preferences" required>
-                        <option value="">Select</option>
-                        <option value="cardio">Cardio</option>
-                        <option value="strength">Strength</option>
-                        <option value="flexibility">Flexibility</option>
-                        <option value="balance">Balance</option>
-                        <option value="endurance">Endurance</option>
-                        <option value="other">Other</option>
-                    </select>
+                    <div className={styles['form-btn']}>
+                        <button className={styles["submit-btn"]} type="submit">Submit</button>
+                        <button className={styles["close-btn"]} onClick={onClose}>Close</button>
+                    </div>
 
                 </form>
+
+                
             </div>
         </div>
     );
