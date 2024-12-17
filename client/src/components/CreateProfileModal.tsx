dfcce557b31129636def6342615cf80d38e83e49
Xavier Figueroa <xfigueroa28@gmail.com>
2024-12-16 22:13:20 -0500

    Added code front-end to send signup form data to back-end

--- client/src/components/CreateProfileModal.tsx
+++ client/src/components/CreateProfileModal.tsx
@@ -1,45 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './LoginModal.module.css';
 
 interface CreateProfileModalProps {
     onClose: () => void;
 }
 
-
 export const CreateProfileModal: React.FC<CreateProfileModalProps> = ({onClose}) => {
+
+    const [formData, setFormData] = React.useState({
+        firstName: '',
+        lastName: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        age: 0,
+        gender: '',
+        weigth: 0,
+        fitnessLevel: '',
+        fitnessGoals: '',
+        exercisePreferences: ''
+    });
+
+    const [error, setError] = useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (formData.password !== formData.confirmPassword) {
+            setError('Passwords do not match');
+            return;
+        }
+        setError('');
+        try {
+            const response = await fetch('/api/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });       
+               
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            const { token } = data;
+            localStorage.setItem('token', token);
+            onClose();
+        } catch (error) {
+            console.error('There was a problem with your fetch operation:', error);
+        }
+            console.log(formData);
+    }
+
+
+
+
     return (
         <div className={styles['login-modal']}>
             <div className={styles['login-modal-content']}>
                 
-                <form action="submit" method="POST">
+                <form onSubmit={handleSubmit}>
+                    
                     <div className={styles["name-inputs"]}>
                         <div>
                             <label htmlFor="first-name">Fist Name:</label>
-                            <input type="text" name="name" id={styles["first-name"]} placeholder="Enter your fist name here" required/>
+                            <input type="text" name="firstName" id="firstName" placeholder="Enter your fist name here" onChange={handleChange} required/>
                         </div>
                         <div>
                             <label htmlFor="last-name">Last Name:</label>
-                            <input type="text" name="name" id={styles["last-name"]} placeholder="Enter your last name here" required/>
+                            <input type="text" name="lastName" id="lastName" placeholder="Enter your last name here" onChange={handleChange} required/>
                         </div>
                     </div>
 
                     <label htmlFor="username">Username:</label>
-                    <input type="text" name="username" id="username" placeholder="Enter your username here" required/>
+                    <input type="text" name="username" id="username" placeholder="Enter your username here" onChange={handleChange} required/>
 
                     <label htmlFor="email">Email Address:</label>
-                    <input type="email" name="email" id="email" placeholder="Enter your email here" required/>
+                    <input type="email" name="email" id="email" placeholder="Enter your email here" onChange={handleChange} required/>
 
                     <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password" placeholder="Enter your password here" required/>
+                    <input type="password" name="password" id="password" placeholder="Enter your password here" onChange={handleChange} required/>
 
                     <label htmlFor="confirm-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-pass" id="confirm-pass" placeholder="Confirm your password here" required/>
+                    <input type="password" name="confirm-pass" id="confirmPassword" placeholder="Confirm your password here" onChange={handleChange} required/>
 
                     <label htmlFor="age">Age:</label>
-                    <input type="number" name="age" id="age" placeholder="Enter your age here" min={1} max={120} />
+                    <input type="number" name="age" id="age" placeholder="Enter your age here" onChange={handleChange} min={1} max={120} />
 
                     <label htmlFor="gender">Gender:</label>
-                    <select id="gender" name="gender" >
+                    <select id="gender" name="gender" onChange={handleChange} >
                         <option value="">Select</option>
                         <option value="male">Male</option>
                         <option value="female">Famale</option>
@@ -48,10 +106,10 @@ export const CreateProfileModal: React.FC<CreateProfileModalProps> = ({onClose})
                     </select>
 
                     <label htmlFor="weight">Weight:</label>
-                    <input type="number" name="weight" id="weight" placeholder="Enter your weight here" min={1} />
+                    <input type="number" name="weight" id="weight" onChange={handleChange} placeholder="Enter your weight here" min={1} />
 
-                    <label htmlFor="fitness-level">Fitness Level:</label>
-                    <select name="fitness-level" id="fitness-level" >
+                    <label htmlFor="fitness-level" >Fitness Level:</label>
+                    <select name="fitness-level" id="fitnessLevel" onChange={handleChange} >
                         <option value="">Select</option>
                         <option value="beginner">Beginner</option>
                         <option value="intermediate">Intermediate</option>
@@ -59,10 +117,10 @@ export const CreateProfileModal: React.FC<CreateProfileModalProps> = ({onClose})
                     </select>
 
                     <label htmlFor="fitness-goals">Fitness Goals</label>
-                    <textarea name="fitness-goals" id="fitness-goals" placeholder="Describe your fitness goals (e.g., lose weight, gain muscle, improve endurance)" rows={4} required></textarea>
+                    <textarea name="fitness-goals" id="fitnessGoals" placeholder="Describe your fitness goals (e.g., lose weight, gain muscle, improve endurance)" rows={4} onChange={handleChange}></textarea>
                     
                     <label htmlFor="exercise-preferences">Exercise Preferences:</label>
-                    <select name="exercise-preferences" id="exercice-preferences" >
+                    <select name="exercise-preferences" id="exercisePreferences" onChange={handleChange} >
                         <option value="">Select</option>
                         <option value="cardio">Cardio</option>
                         <option value="strength">Strength</option>
@@ -71,6 +129,8 @@ export const CreateProfileModal: React.FC<CreateProfileModalProps> = ({onClose})
                         <option value="endurance">Endurance</option>
                         <option value="other">Other</option>
                     </select>
+
+                    {error && <p className={styles["error-message"]}>{error}</p>}
 
                     <div className={styles['form-btn']}>
                         <button className={styles["submit-btn"]} type="submit">Submit</button>
